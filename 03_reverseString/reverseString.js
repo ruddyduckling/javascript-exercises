66d4a39034dfa73c66195707deb5fd89228f5015
ruddyduckling <1039781+ruddyduckling@users.noreply.github.com>
2023-12-18 17:02:34 -0500

    Write function to pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,31 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let reverse = "";
+  for ( i = string.length ; i > 0 ; i--) {
+    reverse = `${reverse}${string[i-1]}`;
+  }
+    return reverse;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+/* A different example showcasing another technique that could have been used
+function reverseString(str) {
+  // Step 1. Use the split() method to return a new array
+  var splitString = str.split(""); // var splitString = "hello".split("");
+  // ["h", "e", "l", "l", "o"]
+
+  // Step 2. Use the reverse() method to reverse the new created array
+  var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+  // ["o", "l", "l", "e", "h"]
+
+  // Step 3. Use the join() method to join all elements of the array into a string
+  var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+  // "olleh"
+  
+  //Step 4. Return the reversed string
+  return joinArray; // "olleh"
+}
+
+reverseString("hello"); */
