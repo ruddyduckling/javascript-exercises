409233186b2e93fee6490f6bebc84583b2211a42
ruddyduckling <1039781+ruddyduckling@users.noreply.github.com>
2023-12-18 16:34:16 -0500

    Pass "ERROR" test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
 const repeatString = function(string, num) {
   let longerString = "";
+  if (num < 0) return longerString = "ERROR";
   for (let i = 0 ; i < num ; i++) {
      longerString += string;
   }
