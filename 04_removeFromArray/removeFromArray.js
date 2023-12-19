fa2c5dd809b8326a60c687fed4e06694fb0af542
ruddyduckling <1039781+ruddyduckling@users.noreply.github.com>
2023-12-18 20:02:50 -0500

    Replace function with filter method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,6 @@
-const removeFromArray = function(userArray) {
-  let removeList = [];
-  for (let i = 1 ; i < arguments.length ; i++) {
-    removeList[removeList.length] = arguments[i];
-  }
-  console.log(removeList);
-
-  let newArray = userArray;
-  for (let j = 0 ; j < removeList.length ; j++) {
-    newArray = newArray.filter(function(value) {
-      return value !== removeList[j]; });
-      console.log(j);
-  }
-  return newArray;
+const removeFromArray = function(userArray, ...args) {
+  return userArray.filter(value => 
+    !args.includes(value));
 }
 // Do not edit below this line
 module.exports = removeFromArray;
