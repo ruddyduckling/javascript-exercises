e2d3dfac9def77edc429a20691ec7266fb16fc19
ruddyduckling <1039781+ruddyduckling@users.noreply.github.com>
2023-12-19 08:07:20 -0500

    Add conditional to satisfy all test conditions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,18 @@
 const sumAll = function(...args) {
-let sum = 0;
-let startNum = Math.min(...args);
-let endNum = Math.max(...args);
+  for (let j=0; j<arguments.length; j++) {
+    if (Math.sign(arguments[j]) !== 1 || !Number.isInteger(arguments[j])) {
+      return "ERROR";
+    }
+  }
 
-while (startNum <= endNum) {
-  sum += startNum;
-  startNum++;
-}
-return sum;
+  const start = Math.min(...args);
+  const end = Math.max(...args);
+  let sum = 0;
+
+  for (let i=start; i<=end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
