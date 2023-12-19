25968e1fd1d93d0383c857dfb94b36f01742a076
ruddyduckling <1039781+ruddyduckling@users.noreply.github.com>
2023-12-19 09:07:46 -0500

    Add functions to satisfy test conditions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celcius = ((fahrenheit - 32) * (5/9));
+  return Number(celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  const fahrenheit = (celcius*(9/5) + 32);
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
