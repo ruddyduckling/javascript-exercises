2afc175cd65b2374bdc6c7bc0dc0f80f07c4402d
ruddyduckling <1039781+ruddyduckling@users.noreply.github.com>
2023-12-18 19:39:20 -0500

    Debug to allow removal of multiple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,16 @@
 const removeFromArray = function(userArray) {
-  let filterList = [];
+  let removeList = [];
   for (let i = 1 ; i < arguments.length ; i++) {
-    filterList[filterList.length] = arguments[i];
+    removeList[removeList.length] = arguments[i];
   }
-  
-  let newArray = [];
-  for (let j = 0 ; j < filterList.length ; j++) {
-    newArray = userArray.filter(function(value) {
-      return value !== filterList[j]; });
-  }
+  console.log(removeList);
 
+  let newArray = userArray;
+  for (let j = 0 ; j < removeList.length ; j++) {
+    newArray = newArray.filter(function(value) {
+      return value !== removeList[j]; });
+      console.log(j);
+  }
   return newArray;
 }
 // Do not edit below this line
